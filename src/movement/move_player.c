915dceeae493adc63168b14c3926785124eb3f69
Vesta Manuyko <vesta.manuyko@gmail.com>
2026-03-13 13:53:34 +0100

    working move_player

--- src/movement/move_player.c
+++ src/movement/move_player.c
@@ -6,7 +6,7 @@
 /*   By: vmanuyko <vmanuyko@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 16:35:18 by vmanuyko          #+#    #+#             */
-/*   Updated: 2026/03/12 18:16:44 by vmanuyko         ###   ########.fr       */
+/*   Updated: 2026/03/13 13:39:06 by vmanuyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ void	move_player(t_user *user)
 {
 	double	new_x;
 	double	new_y;
-	int	dir_x;
-	int	dir_y;
+	double	dir_x;
+	double	dir_y;
 
-	dir_x = user->key.s - user->key.w;
-	dir_y = user->key.a - user->key.d;
-	new_x = user->player_pos.x + dir_x * MOVE_S;
-	new_y = user->player_pos.y + dir_y * MOVE_S;
-	if (!check_collision(user, new_x, new_y))
-	{
+	dir_x = user->key.w - user->key.s;
+	dir_y = user->key.d - user->key.a;
+	new_x = user->player_pos.x + (dir_x * user->dir_vec.x * MOVE_S);
+	new_x += (dir_y * user->plane_vec.x * MOVE_S);
+	new_y = user->player_pos.y + (dir_x * user->dir_vec.y  * MOVE_S);
+	new_y += (dir_y * user->plane_vec.y * MOVE_S);
+	if (!check_collision(user, new_x, user->player_pos.y))
 		user->player_pos.x = new_x;
+	if (!check_collision(user, user->player_pos.x, new_y))
 		user->player_pos.y = new_y;
-	}
 	if (user->key.arr_l)
 		rotate_left(user);
 	if (user->key.arr_r)
